b4d6f43b4e6f7544d19cb098eb3df66004272d14
BestScholar <james.portman88@gmx.com>
2017-03-28 05:48:35 +0200

    ElementNotVisibleException fixed by checking btnG

--- selenium_script.py
+++ selenium_script.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, ElementNotVisibleException
 
 def init_driver():
 	driver = webdriver.Firefox()
@@ -16,7 +16,11 @@ def lookup(driver, query):
 		box = driver.wait.until(EC.presence_of_element_located((By.NAME, "q")))
 		button = driver.wait.until(EC.element_to_be_clickable((By.NAME, "btnK")))
 		box.send_keys(query)
-		button.click()
+		try:
+			button.click()
+		except ElementNotVisibleException:
+			button = driver.wait.until(EC.visibility_of_element_located((By.NAME, "btnG")))
+			button.click()
 	except TimeoutException:
 		print("Box or Button not found in google.com")
 
