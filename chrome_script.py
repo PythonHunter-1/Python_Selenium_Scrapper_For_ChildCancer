b2f4031110079f82e8ccf4e70e22b29196bab5f0
BestScholar <james.portman88@gmx.com>
2017-03-29 03:24:22 +0200

    locate and interact with elements

--- chrome_script.py
+++ chrome_script.py
@@ -5,3 +5,11 @@ browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
 url = 'https://www.lexisnexis.com/hottopics/lnacademic/?verb=sf&amp;sfi=AC00NBGenSrch'
 browser.get(url)
+
+browser.switch_to_frame('mainFrame')
+browser.find_element_by_id('terms').clear()
+browser.find_element_by_id('terms').send_keys('balloon')
+
+browser.find_element_by_xpath('//*[@id="ddlDateOptions"]/option[contains(text(), "Today")]').click()
+browser.find_element_by_xpath('//*[@id="byType"]/option[text()="All News (English)"]').click()
+browser.find_element_by_css_selector('input[type=\"submit\"]').click()
